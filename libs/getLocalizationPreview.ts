00b22ad02e76207d5e05beaaaaa55ea33ee578ab
fmakareev <fmakareev@code-artel.com>
2019-10-25 13:50:21 +0300

    getLocalizationPreview

---
+++ libs/getLocalizationPreview.ts
@@ -0,0 +1,20 @@
+export const getLocalizationPreview = (
+  {
+    previewEn,
+    previewRu,
+    previewUk,
+    preview,
+  }: any,
+  language: string,
+) => {
+  if (language === 'ru') {
+    return previewRu && previewRu.url || preview && preview.url
+  }
+  if (language === 'en') {
+    return previewEn && previewEn.url || preview && preview.url
+  }
+  if (language === 'uk') {
+    return previewUk && previewUk.url || preview && preview.url
+  }
+  return preview.url
+};
